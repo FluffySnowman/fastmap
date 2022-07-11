82c82367f82d66697298de555ef5ad0a7e323692
fluffy <thefluffysnowmanyt@gmail.com>
2022-07-11 09:03:02 +0530

    saving

--- webmap_main.js
+++ webmap_main.js
@@ -6,7 +6,7 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
-app.get('/*', (req, res) => res.send('whats up'));
+app.get('/*', (req, res) => res.send('<h1>hello1aaa</h1>'));
 
 app.listen(42069, () => console.log('running on port 42069'));
 
