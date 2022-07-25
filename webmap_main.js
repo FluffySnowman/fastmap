b213bad83961cfd1cdaac291d9da05c3b035116b
fluffy <thefluffysnowmanyt@gmail.com>
2022-07-25 11:13:35 +0530

    trying to make an expressjs page

--- webmap_main.js
+++ webmap_main.js
@@ -1,17 +1,14 @@
-const express = require('express');
-
+const express = require("express");
+  
 const app = express();
-
-app.set('views', './views');
-app.set('view engine', 'ejs');
-app.use(express.static('./public'));
-
-app.get('/*', (req, res) => res.send('<h1>hello1aaa</h1>'));
-
-app.listen(42069, () => console.log('running on port 42069'));
-
-
-app.get('/*', async (req, res) => {
-    const global = await api.all();
-    res.render('index', { global });
+  
+app.listen(5000, () => {
+  console.log(`Server is up and running on 5000 ...`);
+});
+  
+app.get("/", (req, res) => {
+  
+    var data = "<h1>testing</h1>";
+          
+    res.send(data);
 });
