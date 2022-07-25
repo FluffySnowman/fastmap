744b018040fc06e8a24fe2f258cdd6c64e0aad55
fluffy <thefluffysnowmanyt@gmail.com>
2022-07-25 11:16:28 +0530

    changed startup msg

--- webmap_main.js
+++ webmap_main.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
   
 app.listen(5000, () => {
-  console.log(`Server is up and running on 5000 ...`);
+  console.log(`Fastmap web gui version is running on port 5000 on localhost (127.0.0.1:5000)`);
 });
   
 app.get("/", (req, res) => {
